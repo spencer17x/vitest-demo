4b968fd9a6e73b65b7fc966077b74b1ad9e0d209
Spencer17x <1253478653@qq.com>
2022-04-18 18:06:15 +0800

    feat: test react-router-dom

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,12 +1,43 @@
+/**
+ * vitest
+ */
 import React from 'react';
 import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 
 import Home from './Home';
 
 describe('Home', () => {
   it('first render', () => {
-    const component = render(<Home/>);
+    const history = createMemoryHistory();
+    const component = render(
+      <Router history={history}>
+        <Home/>
+      </Router>
+    );
     expect(component).toMatchSnapshot();
   });
 });
+
+
+// /**
+//  * jest
+//  */
+// import { render } from '@testing-library/react';
+// import { createMemoryHistory } from 'history';
+// import Home from './Home';
+// import { Router } from 'react-router-dom';
+//
+// describe('Home', function () {
+//   it('first render', () => {
+//     const history = createMemoryHistory();
+//     const component = render(
+//       <Router history={history}>
+//         <Home/>
+//       </Router>
+//     );
+//     expect(component).toMatchSnapshot();
+//   });
+// });
